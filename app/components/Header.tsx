8e4ee2b5594d4e96ce373d801c846275b5ad8ee3
marcelinus99 <marc.gonzalez.moratona@estudiantat.upc.edu>
2023-03-07 01:21:36 +0100

    feat: translations

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,25 +1,41 @@
 import Link from 'next/link';
 
 import { useState } from 'react';
- import Menu from './Menu';
- import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
- import { faBars } from '@fortawesome/free-solid-svg-icons';
+import Menu from './Menu';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import LanguageSelector from './LanguageSelector';
+import useTranslation from 'next-translate/useTranslation';
 
 const Header = () => {
-
   const [isMenuOpen, toggleMenu] = useState(false);
+  const { t } = useTranslation('common');
 
   return (
-    <header className="flex flex-col bg-black text-white text-lg p-6">
-      <div className="flex items-center justify-between">
+    <header className="flex flex-col bg-black text-white text-lg p-6 sm:flex-row">
       <Link href="/">HORIZONS GAMES</Link>
-      <LanguageSelector/>
-      <div className="flex" onClick={()=>toggleMenu(!isMenuOpen)}>
-        <FontAwesomeIcon icon={faBars} className="text-xl" />
+      <div className="flex items-center justify-between sm:mx-12">
+        <nav className="hidden sm:block">
+          <ul className="flex">
+            <Link href="/">
+              <li className="mx-4">{t('HOME')}</li>
+            </Link>
+            <Link href="/team">
+              <li className="mx-4">{t('TEAM')}</li>
+            </Link>
+          </ul>
+        </nav>
+        <div className="hidden sm:block absolute right-4">
+          <LanguageSelector />
+        </div>
+        <div
+          className="flex sm:hidden absolute right-8"
+          onClick={() => toggleMenu(!isMenuOpen)}
+        >
+          <FontAwesomeIcon icon={faBars} className="text-xl" />
+        </div>
       </div>
-      </div>
-      <Menu isMenuOpen={isMenuOpen} toggleMenu={toggleMenu}/>
+      <Menu isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
     </header>
   );
 };
--- app/components/LanguageSelector.tsx
+++ app/components/LanguageSelector.tsx
@@ -1,50 +1,72 @@
 import * as Select from '@radix-ui/react-select';
-import { useState } from 'react';
 import { ChevronDownIcon } from '@radix-ui/react-icons';
+import { useEffect, useState } from 'react';
 import useTranslation from 'next-translate/useTranslation';
-import Link from 'next/link';
-
+import { useRouter } from 'next/router';
 
 const LanguageSelector = () => {
-    const { t } = useTranslation('common');
-    const [value, setValue] = useState(t('CURRENT_LANGUAGE'));
+  const { t } = useTranslation('common');
+  const currentLanguage = t('CURRENT_LANGUAGE');
+  const [value, setValue] = useState(currentLanguage);
+  const router = useRouter();
 
-return (
-    <Select.Root value={value} onValueChange={setValue} >
-        <Select.Trigger className="flex items-center px-2">
-            <Select.Value aria-label={value}>
-            {value}
-            </Select.Value>
-            <Select.Icon className="mx-2">
-                <ChevronDownIcon className='h-4 w-4'/>
-            </Select.Icon>
-        </Select.Trigger>
-        <Select.Content position="popper" className="bg-black rounded-lg">
-            <Select.Viewport>
-                <Link href="/ca" locale="ca">Catalan
-                    <Select.Item value="Catalan" className="px-2 m-3 hover:bg-white hover:text-black">
-                        <Select.ItemText></Select.ItemText>
-                    </Select.Item>
-                </Link>
-                <Link href="/en" locale="en">English
-                    <Select.Item value="English" className="px-2 m-3 hover:bg-white hover:text-black">
-                        <Select.ItemText></Select.ItemText>
-                    </Select.Item>
-                </Link>
-                <Link href="/fr" locale="fr">French
-                    <Select.Item value="French" className="px-2 m-3 hover:bg-white hover:text-black">
-                        <Select.ItemText></Select.ItemText>
-                    </Select.Item>
-                </Link>
-                <Link href="/es" locale="es">Spanish
-                    <Select.Item value="Spanish" className="px-2 m-3 hover:bg-white hover:text-black">
-                        <Select.ItemText></Select.ItemText>
-                    </Select.Item>
-                </Link>
-            </Select.Viewport>
-        </Select.Content>
+  useEffect(() => {
+    if (value === 'Català')
+      router.push(router.pathname, router.pathname, { locale: 'ca' });
+    else if (value === 'English')
+      router.push(router.pathname, router.pathname, { locale: 'en' });
+    else if (value === 'Español')
+      router.push(router.pathname, router.pathname, { locale: 'es' });
+    else if (value === 'Français')
+      router.push(router.pathname, router.pathname, { locale: 'fr' });
+  }, [value]);
+
+  return (
+    <Select.Root value={value} onValueChange={setValue}>
+      <Select.Trigger className="flex items-center p-2">
+        <Select.Value aria-label={value}>{value}</Select.Value>
+        <Select.Icon className="mx-2">
+          <ChevronDownIcon className="h-4 w-4" />
+        </Select.Icon>
+      </Select.Trigger>
+      <Select.Content position="popper" className="bg-black">
+        <Select.Viewport className="flex flex-col">
+          {currentLanguage !== 'Català' && (
+            <Select.Item
+              value="Català"
+              className="hover:bg-white hover:text-black pl-2 pr-8 py-1"
+            >
+              <Select.ItemText>Català</Select.ItemText>
+            </Select.Item>
+          )}
+          {currentLanguage !== 'English' && (
+            <Select.Item
+              value="English"
+              className="hover:bg-white hover:text-black pl-2 pr-8 py-1"
+            >
+              <Select.ItemText>English</Select.ItemText>
+            </Select.Item>
+          )}
+          {currentLanguage !== 'Español' && (
+            <Select.Item
+              value="Español"
+              className="hover:bg-white hover:text-black pl-2 pr-8 py-1"
+            >
+              <Select.ItemText>Español</Select.ItemText>
+            </Select.Item>
+          )}
+          {currentLanguage !== 'Français' && (
+            <Select.Item
+              value="Français"
+              className="hover:bg-white hover:text-black pl-2 pr-8 py-1"
+            >
+              <Select.ItemText>Français</Select.ItemText>
+            </Select.Item>
+          )}
+        </Select.Viewport>
+      </Select.Content>
     </Select.Root>
-)
-}
+  );
+};
 
 export default LanguageSelector;
