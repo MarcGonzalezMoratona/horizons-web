8d63504330c36456342c793d459babd91a9b698f
MarcGonzalezMoratona <m.gonzalez@shopfully.com>
2023-03-15 12:50:43 +0100

    fix: post example comments

--- app/data/posts.example.js
+++ app/data/posts.example.js
@@ -7,9 +7,9 @@ export const posts = [
     // Subtitle of the post
     subtitle: `Horizons Team begins development of Axolotl Project`,
     // Image file name for the post preview
-    caption: "smash.jpg",
+    caption: "smash.jpg", // [OPTIONAL]
     // Alternate text for the image file
-    alt: "smash bros",
+    alt: "smash bros", // [OPTIONAL]
     // Image file of the author's profile image
     profile: "MarcGonzalez.jpg",
     // Author's profile image file name
@@ -20,19 +20,19 @@ export const posts = [
     paragraphs: [
       {
         // Heading of the paragraph
-        heading: "First heading",
+        heading: "First heading", // [OPTIONAL]
         // Content of the paragraph
         content: `Content of the first paragraph.`,
         // Image file name for the paragraph
-        image: "../horizons.svg",
+        image: "../horizons.svg", // [OPTIONAL]
         // URL of a video that should be embedded with the paragraph
         // Supported video types: YouTube, Vimeo, Dailymotion,
         // and other video hosting services that support embeddable iframes.
-        video: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+        video: "https://www.youtube.com/embed/dQw4w9WgXcQ", // [OPTIONAL]
         // Name of a document file that can be downloaded
-        document: "VS1.pdf",
+        document: "VS1.pdf", // [OPTIONAL]
         // Text of the call-to-action (CTA) for the document download link
-        documentCTA: "Download GVD",
+        documentCTA: "Download GVD", // [OPTIONAL]
       },
     ],
   },
