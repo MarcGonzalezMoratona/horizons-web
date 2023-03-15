1691d2d662b0d683d5d541beaef127f0a926de01
MarcGonzalezMoratona <m.gonzalez@shopfully.com>
2023-03-15 13:21:01 +0100

    fix: header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,16 +1,15 @@
-import Link from 'next/link';
-
-import { useState } from 'react';
-import Menu from './Menu';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-import LanguageSelector from './LanguageSelector';
-import useTranslation from 'next-translate/useTranslation';
-import { HorizonsWhite } from './Horizons';
+import Link from "next/link";
+import { useState } from "react";
+import Menu from "./Menu";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import LanguageSelector from "./LanguageSelector";
+import useTranslation from "next-translate/useTranslation";
+import { HorizonsWhite } from "./Horizons";
 
 const Header = () => {
   const [isMenuOpen, toggleMenu] = useState(false);
-  const { t } = useTranslation('common');
+  const { t } = useTranslation("common");
 
   return (
     <header className="flex flex-col bg-black text-white text-lg p-6 sm:flex-row">
@@ -21,14 +20,17 @@ const Header = () => {
         <nav className="hidden sm:block sm:mx-12">
           <ul className="flex">
             <Link href="/team">
-              <li className="mx-4">{t('TEAM')}</li>
+              <li className="mx-4">{t("TEAM")}</li>
             </Link>
             <Link
               href={{
-                pathname: '/blog',
+                pathname: "/blog",
               }}
             >
-              <li className="mx-4">{t('BLOG')}</li>
+              <li className="mx-4">{t("BLOG")}</li>
+            </Link>
+          </ul>
+        </nav>
         <div className="hidden sm:block absolute right-4">
           <LanguageSelector />
         </div>
