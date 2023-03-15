bba822e251033e5173fe3e824eb0472cc2b115a1
MarcGonzalezMoratona <m.gonzalez@shopfully.com>
2023-03-15 09:47:40 +0100

    feat: comment explanation of JSON

--- app/data/posts.js
+++ app/data/posts.js
@@ -1,14 +1,14 @@
 export const posts = [
   {
     id: 1,
-    title: 'Axolotl project: the development starts',
+    title: "Axolotl project: the development starts",
     subtitle: `Horizons Team begins development of Axolotl Project`,
-    profile: 'MarcGonzalez.jpg',
-    author: 'Marc González',
-    date: '14/03/2023',
+    profile: "MarcGonzalez.jpg",
+    author: "Marc González",
+    date: "14/03/2023",
     paragraphs: [
       {
-        heading: 'The project comes up to life',
+        heading: "The project comes up to life",
         content: `On March 14th, the Horizons team kicked off the development of their first delivery for Axolotl Project, a new video game. 
         The team, comprised of 24 students from the UPC School's first Master's Degree in Advanced Programming for AAA video games class, 
         has been preparing for this moment since they first met in October 2022. In the intervening months, 
@@ -16,36 +16,36 @@ export const posts = [
         A lot of things have been going on and now it's time to start applying knowledge to the real project.`,
       },
       {
-        heading: 'The first vertical slice',
+        heading: "The first vertical slice",
         content: `The first major milestone in the project is the creation of a "vertical slice" 
         - a playable prototype that lays the foundation for the game's structure. 
         Over the past few weeks, the team has been hard at work on concept discovery, defining the game's art style, lore, and core functionality.
         The goal of this delivery is to present a playable prototype that showcases the game's core gameplay and application structure.`,
-        image: '../horizons.svg',
+        image: "../horizons.svg",
       },
       {
-        heading: 'Team methodology',
+        heading: "Team methodology",
         content: `To achieve this and subsequent milestones, the team has divided into several smaller teams, 
         each with its own area of focus: Art, Engine, Game Design, Level Design, User Interface, Public Relations, Graphics, and Physics.
         Each team is led by a designated coordinator who manages tasks and ensures everyone is working together towards the same goals.
         Team members are encouraged to change roles between milestones to gain experience in different areas of the game development process.
         If you're interested in following along with the team's progress, you can check out our social media channels.
         Stay tuned for more exciting news!`,
-        video: 'https://www.youtube.com/embed/dQw4w9WgXcQ',
+        video: "https://www.youtube.com/embed/dQw4w9WgXcQ",
       },
     ],
   },
   {
     id: 2,
-    title: 'The GVD is ready!',
-    subtitle: 'Horizons team publishes the game vision document of their game',
-    profile: 'MarcGonzalez.jpg',
-    author: 'Marc González',
-    date: '15/03/2023',
+    title: "The GVD is ready!",
+    subtitle: "Horizons team publishes the game vision document of their game",
+    profile: "MarcGonzalez.jpg",
+    author: "Marc González",
+    date: "15/03/2023",
     paragraphs: [
       {
         heading:
-          'A detailed overview of features, mechanics, and intended audience for upcoming game',
+          "A detailed overview of features, mechanics, and intended audience for upcoming game",
         content: `The Horizons team is excited to announce that they have published the GVD (Game Vision Document) for their upcoming game. 
         The GVD is an essential document that outlines the game's concept, target audience, features, and gameplay mechanics.
         The team has been working hard over the past few months to define the vision for Axolotl project and bring it to life.
@@ -55,21 +55,21 @@ export const posts = [
         `,
       },
       {
-        heading: '',
+        heading: "",
         content: `
         In the coming weeks, the team plans to share more updates on the development of Axolotl, including progress on the game's 
         vertical slices. We invite everyone to follow our social media channels to stay up-to-date on the latest news and announcements. 
         The Horizons team is thrilled to share their journey with the community and can't wait to bring Axolotl project to life!`,
       },
       {
-        heading: '',
+        heading: "",
         content: ` With the GVD now published, the team is one step closer to making their vision a reality. The document provides a detailed overview 
         of the game's core features and mechanics, as well as its intended audience and overall theme.
         The Horizons team is excited to share their progress with the community and hopes that the GVD will provide insight into the upcoming game.
         The team also acknowledges that the GVD is not set in stone and is subject to change as development progresses. 
         However, we hope that it provides a solid foundation for the game's development and gives players an idea of what to expect.`,
-        document: 'VS1.pdf',
-        documentCTA: 'Download GVD',
+        document: "VS1.pdf",
+        documentCTA: "Download GVD",
       },
     ],
   },
