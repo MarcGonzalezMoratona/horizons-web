a6b252b38953af079b7a3db18c21cb7c11937c06
marcelinus99 <marc.gonzalez.moratona@estudiantat.upc.edu>
2023-03-25 15:45:32 +0100

    fix: button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -13,9 +13,12 @@ export default function Button({
 }: ButtonProps) {
   return (
     <button
-      className={`font-bold tracking-wider px-6 py-3 mx-2 my-4 rounded-md bg-${variant}-500 text-neutral-100 
-      dark:bg-${variant}-500 dark:text-neutral-100 border-t-2 border-b-4 border-t-${variant}-300 border-b-${variant}-800 
-      hover:translate-y-0.5 transition-transform duration-300 ${className}`}
+      className={`font-bold tracking-wider px-6 py-3 mx-2 my-4 rounded-md  text-neutral-100 
+       dark:text-neutral-100 border-t-2 border-b-4 hover:translate-y-0.5 transition-transform duration-300 
+       ${
+         variant === 'primary' &&
+         'bg-primary-500 border-t-primary-300 border-b-primary-800 dark:bg-primary-500'
+       } ${className}`}
     >
       {children}
       {icon}
