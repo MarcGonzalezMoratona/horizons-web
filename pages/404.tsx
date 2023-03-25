ecd7d750247a6889c84bf1b56b33cbcfe27af0f6
marcelinus99 <marc.gonzalez.moratona@estudiantat.upc.edu>
2023-03-25 19:57:50 +0100

    feat: add prettier plugin for TW

--- pages/404.tsx
+++ pages/404.tsx
@@ -26,8 +26,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        <div className="flex flex-col lg:flex-row items-center justify-center min-h-screen">
-          <div className="relative w-[250px] h-[300px] sm:w-[450px] sm:h-[550px] lg:w-[500px] lg:h-[600px] my-8 sm:my-0">
+        <div className="flex min-h-screen flex-col items-center justify-center lg:flex-row">
+          <div className="relative my-8 h-[300px] w-[250px] sm:my-0 sm:h-[550px] sm:w-[450px] lg:h-[600px] lg:w-[500px]">
             <Image
               src="/darthVader.png"
               alt="LEGO darth vader"
@@ -35,17 +35,17 @@ export default function Home() {
               className="object-contain"
             />
           </div>
-          <div className="flex flex-col justify-center items-center lg:items-start text-center sm:text-left mx-8 h-64 sm:h-96 lg:w-1/2">
-            <h1 className="font-bold text-2xl sm:text-4xl lg:text-6xl">
+          <div className="mx-8 flex h-64 flex-col items-center justify-center text-center sm:h-96 sm:text-left lg:w-1/2 lg:items-start">
+            <h1 className="text-2xl font-bold sm:text-4xl lg:text-6xl">
               {t('NOT_FOUND').toUpperCase()}
             </h1>
-            <h2 className="text-lg lg:text-2xl my-8">
+            <h2 className="my-8 text-lg lg:text-2xl">
               {t('404_MESSAGE').toUpperCase()}
             </h2>
             <Link href="/">
               <button
-                className="bg-red-700 font-semibold p-4 w-48 sm:w-96 text-neutral-100 rounded-md 
-              border-b-4 border-t-2 border-t-red-500 border-b-red-900"
+                className="w-48 rounded-md border-b-4 border-t-2 border-t-red-500 border-b-red-900 bg-red-700 
+              p-4 font-semibold text-neutral-100 sm:w-96 sm:transition-transform sm:duration-300 sm:hover:translate-y-0.5"
               >
                 {t('GO_HOME').toUpperCase()}
               </button>
