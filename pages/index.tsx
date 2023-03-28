effd9c1e42a349b3c703d9a8e2f169bf60242bab
MarcGonzalezMoratona <m.gonzalez@shopfully.com>
2023-03-28 15:40:13 +0200

    fix: change axolotl engine version

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,29 @@
-import { GearIcon } from '@radix-ui/react-icons';
-import useTranslation from 'next-translate/useTranslation';
-import Head from 'next/head';
-import { useEffect } from 'react';
-import Carousel from '../app/components/Carousel';
-import ComingSoon from '../app/components/ComingSoon';
-import { GlowingButton } from '../app/components/GlowingButton';
-import Layout from '../app/components/Layout';
-import SocialNetworks from '../app/components/SocialNetworks';
-import { socialNetworks } from '../app/data/socialNetworks';
-import { usePageHandler } from '../app/hooks/usePageInfo';
+import { GearIcon } from "@radix-ui/react-icons";
+import useTranslation from "next-translate/useTranslation";
+import Head from "next/head";
+import { useEffect } from "react";
+import Carousel from "../app/components/Carousel";
+import ComingSoon from "../app/components/ComingSoon";
+import { GlowingButton } from "../app/components/GlowingButton";
+import Layout from "../app/components/Layout";
+import SocialNetworks from "../app/components/SocialNetworks";
+import { socialNetworks } from "../app/data/socialNetworks";
+import { usePageHandler } from "../app/hooks/usePageInfo";
 
 export default function Home() {
-  const { t } = useTranslation('common');
+  const { t } = useTranslation("common");
   const PageHandler = usePageHandler();
 
   useEffect(() => {
-    PageHandler('landing');
+    PageHandler("landing");
   });
 
   const images = [
-    'legoStarWarsPlanet.jpg',
-    'darthVader.jpg',
-    'ahsokaTano.jpg',
-    'stormTroopers.jpg',
-    'battle.jpg',
+    "legoStarWarsPlanet.jpg",
+    "darthVader.jpg",
+    "ahsokaTano.jpg",
+    "stormTroopers.jpg",
+    "battle.jpg",
   ];
 
   return (
@@ -42,10 +42,10 @@ export default function Home() {
         <ComingSoon />
         <SocialNetworks networks={socialNetworks} className="my-4" />
         <GlowingButton
-          mainText="Axolotl Engine 2.0.0"
-          CTAText={t('SEE_NEWS')}
+          mainText="Axolotl Engine 3.0.0"
+          CTAText={t("SEE_NEWS")}
           icon={
-            <GearIcon className="h-5 w-5 text-secondary-600 animate-spin sm:animate-none sm:group-hover:animate-spin transition duration-200" />
+            <GearIcon className="h-5 w-5 animate-spin text-secondary-600 transition duration-200 sm:animate-none sm:group-hover:animate-spin" />
           }
         />
       </Layout>
