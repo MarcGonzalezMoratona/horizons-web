9cab1d887b245a0c9eb46496fff12ba67c7c8b75
MarcGonzalezMoratona <m.gonzalez@shopfully.com>
2023-03-28 15:44:12 +0200

    fix: remove carousel

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,5 +1,5 @@
-import Footer from './Footer';
-import Header from './Header';
+import Footer from "./Footer";
+import Header from "./Header";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -9,7 +9,7 @@ const Layout = (props: LayoutProps) => {
   return (
     <>
       <Header className="font-montserrat" />
-      <main className="flex flex-col font-montserrat items-center justify-center text-neutral-800 bg-neutral-100 dark:text-neutral-100 dark:bg-neutral-900">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-neutral-100 font-montserrat text-neutral-800 dark:bg-neutral-900 dark:text-neutral-100">
         {props.children}
       </main>
       <Footer />
