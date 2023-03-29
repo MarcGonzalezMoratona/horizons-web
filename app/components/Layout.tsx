6f88eacc4821836cb981d8ada7452dedb658bfdf
marcelinus99 <marc.gonzalez.moratona@estudiantat.upc.edu>
2023-03-29 20:46:30 +0200

    feat: carousel and images added

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,5 +1,5 @@
-import Footer from "./Footer";
-import Header from "./Header";
+import Footer from './Footer';
+import Header from './Header';
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -9,7 +9,7 @@ const Layout = (props: LayoutProps) => {
   return (
     <>
       <Header className="font-montserrat" />
-      <main className="flex min-h-screen flex-col items-center justify-center bg-neutral-100 font-montserrat text-neutral-800 dark:bg-neutral-900 dark:text-neutral-100">
+      <main className="flex flex-col items-center justify-center bg-neutral-100 font-montserrat text-neutral-800 dark:bg-neutral-900 dark:text-neutral-100">
         {props.children}
       </main>
       <Footer />
