1cb73356cc56a9d34411f58727f2450dd79fb595
marcelinus99 <marcgonzalezmoratona@gmail.com>
2023-09-11 23:21:08 +0200

    feat: add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,8 @@ import '@fortawesome/fontawesome-svg-core/styles.css';
 import { ThemeContextProvider } from '../store/ThemeContext';
 import { PageContextProvider } from '../store/PageContext';
 import { DeviceContextProvider } from '../store/DeviceContext';
+import { Analytics } from '@vercel/analytics/react';
+
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -13,6 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <PageContextProvider>
         <ThemeContextProvider>
           <Component {...pageProps} />
+          <Analytics />
         </ThemeContextProvider>
       </PageContextProvider>
     </DeviceContextProvider>
