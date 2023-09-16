81a6b33061ded1c72a6cddf0043301c572bd2d5f
marcelinus99 <marcgonzalezmoratona@gmail.com>
2023-09-16 11:25:20 +0200

    fix: roles

--- app/data/team.js
+++ app/data/team.js
@@ -3,23 +3,13 @@ export const team = {
     {
       name: 'David Castro',
       image: 'DavidCastro.jpg',
-      role: 'Gameplay Programmer',
       networks: {
         github: 'https://github.com/DADSILVER',
       },
     },
     {
-      name: 'David García',
-      image: 'DavidGarcia.jpg',
-      role: 'Gameplay Programmer',
-      networks: {
-        github: 'https://github.com/DGMC117',
-      },
-    },
-    {
       name: 'Diego Mejía',
       image: 'DiegoMejia.jpg',
-      role: 'Gameplay Programmer',
       networks: {
         github: 'https://github.com/diegusmejia117',
       },
@@ -27,7 +17,6 @@ export const team = {
     {
       name: 'Gerard Queralt',
       image: 'GerardQueralt.jpg',
-      role: 'Gameplay Programmer',
       networks: {
         github: 'https://github.com/gerard-queralt',
         linkedin:
@@ -37,7 +26,6 @@ export const team = {
     {
       name: 'Miquel Prieto',
       image: 'MiquelPrieto.jpg',
-      role: 'Gameplay Programmer',
       networks: {
         github: 'https://github.com/MacXxs',
         linkedin:
@@ -45,19 +33,8 @@ export const team = {
       },
     },
     {
-      name: 'Santiago Carbó',
-      image: 'SantiagoCarbo.jpg',
-      role: 'Gameplay Programmer',
-      networks: {
-        github: 'https://github.com/sncarbo',
-        linkedin: 'https://www.linkedin.com/in/santiago-carb%C3%B3-00805510b',
-        website: 'https://santiago-carbo.com',
-      },
-    },
-    {
       name: 'Pedro González-Anta',
       image: 'PeterGonzalez.jpg',
-      role: 'Gameplay Programmer',
       networks: {
         github: 'https://github.com/Piterga12',
       },
@@ -65,32 +42,30 @@ export const team = {
     {
       name: 'Alexis Dieu',
       image: 'AlexisDieu.jpg',
-      role: 'Gameplay Programmer',
       networks: {
         github: 'https://github.com/AlexisDieu',
         linkedin: 'https://www.linkedin.com/in/alexis-dieu-705ab81a3',
       },
     },
     {
-      name: 'Andrés Machado',
-      image: 'AndresMachado.jpg',
-      role: 'Gameplay Programmer',
-      networks: {
-        github: 'https://github.com/AndresM2',
-      },
-    },
-    {
       name: 'David Martín',
       image: 'DavidMartin.jpg',
-      role: 'Gameplay Programmer',
       networks: {
         github: 'https://github.com/Dedado3000',
       },
     },
     {
+      name: 'Marc Alcón',
+      image: 'MarcAlcon.jpg',
+      networks: {
+        github: 'https://github.com/markmeliaa',
+        linkedin: 'https://www.linkedin.com/in/markmeliaa',
+        website: 'https://markmeliaa.github.io',
+      },
+    },
+    {
       name: 'Sara Izquierdo',
       image: 'SaraIzquierdo.jpg',
-      role: 'Gameplay Programmer',
       networks: {
         github: 'https://github.com/AuxPort66',
         linkedin: 'https://www.linkedin.com/in/sara-izquierdo-elfau-285109222',
@@ -98,17 +73,16 @@ export const team = {
       },
     },
     {
-      name: 'Arnau Pérez',
-      image: 'ArnauPerez.jpg',
-      role: 'Gameplay Programmer',
+      name: 'Eric Dacal',
+      image: 'EricDacal.jpg',
       networks: {
-        github: 'https://github.com/Mondagh',
+        github: 'https://github.com/ericdacal',
+        linkedin: 'https://www.linkedin.com/in/erdasa',
       },
     },
     {
       name: 'Xavier Llubés',
       image: 'XavierLlubes.jpg',
-      role: 'Gameplay Programmer',
       networks: {
         github: 'https://github.com/xavi1111',
       },
@@ -116,7 +90,6 @@ export const team = {
     {
       name: 'Oriol Murcia',
       image: 'OriolMurcia.jpg',
-      role: 'Gameplay Programmer',
       networks: {
         github: 'https://github.com/UriMurcia',
         linkedin: 'https://www.linkedin.com/in/oriol-murcia-catalan',
@@ -126,7 +99,6 @@ export const team = {
     {
       name: 'Marc González',
       image: 'MarcGonzalez.jpg',
-      role: 'Gameplay Programmer',
       networks: {
         github: 'https://www.github.com/MarcGonzalezMoratona',
         linkedin: 'https://www.linkedin.com/in/marc-gonzalez-moratona',
@@ -136,7 +108,6 @@ export const team = {
     {
       name: 'Oriol Sors',
       image: 'OriolSors.jpg',
-      role: 'Gameplay Programmer',
       networks: {
         github: 'https://github.com/OriolSors',
         linkedin: 'https://www.linkedin.com/in/oriol-sors-vidal-4361491b9',
@@ -145,7 +116,6 @@ export const team = {
     {
       name: 'Sandra Campaña',
       image: 'SandraCampaña.jpg',
-      role: 'Gameplay Programmer',
       networks: {
         github: 'https://github.com/csandra24',
       },
@@ -153,7 +123,6 @@ export const team = {
     {
       name: 'Rodrigo Samayoa',
       image: 'RodrigoSamayoa.jpg',
-      role: 'Gameplay Programmer',
       networks: {
         github: 'https://github.com/totisama',
         linkedin: 'https://www.linkedin.com/in/rodrigo-samayoa-526916196',
@@ -162,7 +131,6 @@ export const team = {
     {
       name: 'Pablo Cebollada',
       image: 'PabloCebollada.jpg',
-      role: 'Gameplay Programmer',
       networks: {
         github: 'https://github.com/PaablooCH',
       },
@@ -170,44 +138,45 @@ export const team = {
     {
       name: 'Alejandro Matus',
       image: 'AlejandroMatus.jpg',
-      role: 'Gameplay Programmer',
       networks: {
         github: 'https://github.com/Vlukkee',
       },
     },
     {
-      name: 'Marc Alcón',
-      image: 'MarcAlcon.jpg',
-      role: 'Gameplay Programmer',
+      name: 'Santiago Carbó',
+      image: 'SantiagoCarbo.jpg',
       networks: {
-        github: 'https://github.com/markmeliaa',
-        linkedin: 'https://www.linkedin.com/in/markmeliaa',
-        website: 'https://markmeliaa.github.io',
+        github: 'https://github.com/sncarbo',
+        linkedin: 'https://www.linkedin.com/in/santiago-carb%C3%B3-00805510b',
+        website: 'https://santiago-carbo.com',
       },
     },
     {
       name: 'Jesús Miguel Adrián',
       image: 'JesusMiguelAdrian.jpg',
-      role: 'Gameplay Programmer',
       networks: {
         github: 'https://github.com/elbuho12Jesus',
       },
     },
     {
+      name: 'David García',
+      image: 'DavidGarcia.jpg',
+      networks: {
+        github: 'https://github.com/DGMC117',
+      },
+    },
+    {
       name: 'Jan Marc Costa',
       image: 'JanMarcCosta.jpg',
-      role: 'Gameplay Programmer',
       networks: {
         github: 'https://github.com/TheGeminis',
       },
     },
     {
-      name: 'Eric Dacal',
-      image: 'EricDacal.jpg',
-      role: 'Gameplay Programmer',
+      name: 'Andrés Machado',
+      image: 'AndresMachado.jpg',
       networks: {
-        github: 'https://github.com/ericdacal',
-        linkedin: 'https://www.linkedin.com/in/erdasa',
+        github: 'https://github.com/AndresM2',
       },
     },
   ],
