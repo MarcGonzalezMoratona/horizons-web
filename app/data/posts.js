a15084f51506196fdc1a085e608815a22d816171
marcelinus99 <marcgonzalezmoratona@gmail.com>
2023-11-10 11:53:29 +0100

    refactor: remove posts from starfall rebellion

--- app/data/posts.js
+++ app/data/posts.js
@@ -1,31 +1,6 @@
 export const posts = [
   {
     id: 1,
-    slug: 'axolotl-project-the-development-starts',
-    title: 'Axolotl project: the development starts',
-    subtitle: 'Horizons Team begins development of Axolotl Project',
-    profile: 'MarcGonzalez.jpg',
-    author: 'Marc González',
-    date: '14/03/2023',
-  },
-  {
-    id: 2,
-    slug: 'the-gvd-is-ready',
-    title: 'The GVD is ready!',
-    subtitle: 'Horizons team publishes the game vision document of their game',
-    profile: 'MarcGonzalez.jpg',
-    caption: 'bix.png',
-    author: 'Marc González',
-    date: '25/03/2023',
-    paragraphs: [
-      {
-        document: 'GVD.pdf',
-        documentCTA: 'Download GVD',
-      },
-    ],
-  },
-  {
-    id: 3,
     slug: 'adding-reflection-to-our-scripts',
     title: 'The engineering process: adding reflection to our scripts',
     subtitle:
